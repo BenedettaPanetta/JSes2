b587fbe55fe69e511a2d7950a088c1a2cec29933
BenedettaPanetta <panetta.benedettapia@studenti.salesianisesto.it>
2020-11-22 12:21:57 +0100

    edit dom with jquery

---
+++ es2.js
@@ -0,0 +1,17 @@
+/*
+var addBtn = function(){
+    var btn = document.createElement("button"); //creo dinamicamente un oggetto di tipo bottone
+    btn.innerHTML = "NON CLICCARE";
+    document.getElementById("body1").append(btn); //aggiunge button al body
+    btn.addEventListener("click", changeColor);
+}
+var changeColor = function (){
+    console.log("ciao");
+    document.getElementById("div1").setAttribute("style", "background-color: rgb(115, 224, 238)"); //modifica colore div
+    span1.innerHTML = "CUCU"; //modifica testo span
+    document.getElementById("div1").innerHTML=("<h1>Ti ho detto di non premere il bottone</h1>");
+}
+// var button=document.getElementById("btn1"); //ascoltatore per il bottone
+// button.addEventListener("click", changeColor)
+document.addEventListener("DOMContentLoaded", addBtn); //evento dell'oggetto dom, quando esso viene caricato -> richiama funzione
+*/
